53eb561350bf3f62edeca8f208dd71017b21d412
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2023-10-13 20:05:21 +0300

    fix questions

--- main.js
+++ main.js
@@ -1,23 +1,80 @@
 $('.answer').hide(0);
 
+let PlusRotate = false;
+
 $('#plus1').click(function(){
-    $('#answer1').slideToggle(300)
-})
+    if(PlusRotate == false){
+        $('#plus1').css('transform', 'rotate(45deg)');
+        PlusRotate = true;
+    }else{
+        $('#plus1').css('transform', 'rotate(0deg)');
+        PlusRotate = false;
+    }
+    $('#answer1').slideToggle(300);
+});
+
+let PlusRotate2 = false;
+
 $('#plus2').click(function(){
-    $('#answer2').slideToggle(300)
-})
+    if(PlusRotate2 == false){
+        $('#plus2').css('transform', 'rotate(45deg)');
+        PlusRotate2 = true;
+    }else{
+        $('#plus2').css('transform', 'rotate(0deg)');
+        PlusRotate2 = false;
+    }
+    $('#answer2').slideToggle(300);
+});
+
+let PlusRotate3 = false;
+
 $('#plus3').click(function(){
-    $('#answer3').slideToggle(300)
-})
+    if(PlusRotate3 == false){
+        $('#plus3').css('transform', 'rotate(45deg)');
+        PlusRotate3 = true;
+    }else{
+        $('#plus3').css('transform', 'rotate(0deg)');
+        PlusRotate3 = false;
+    }
+    $('#answer3').slideToggle(300);
+});
+
+let PlusRotate4 = false;
+
 $('#plus4').click(function(){
-    $('#answer4').slideToggle(300)
-})
+    if(PlusRotate4 == false){
+        $('#plus4').css('transform', 'rotate(45deg)');
+        PlusRotate4 = true;
+    }else{
+        $('#plus4').css('transform', 'rotate(0deg)');
+        PlusRotate4 = false;
+    }
+    $('#answer4').slideToggle(300);
+});
+
+let PlusRotate5 = false;
+
 $('#plus5').click(function(){
-    $('#answer5').slideToggle(300)
-})
-$('#plus5').click(function(){
-    $('#answer5').slideToggle(300)
-})
+    if(PlusRotate5 == false){
+        $('#plus5').css('transform', 'rotate(45deg)');
+        PlusRotate5 = true;
+    }else{
+        $('#plus5').css('transform', 'rotate(0deg)');
+        PlusRotate5 = false;
+    }
+    $('#answer5').slideToggle(300);
+});
+
+let PlusRotate6 = false;
+
 $('#plus6').click(function(){
-    $('#answer6').slideToggle(300)
-})
+    if(PlusRotate6 == false){
+        $('#plus6').css('transform', 'rotate(45deg)');
+        PlusRotate6 = true;
+    }else{
+        $('#plus6').css('transform', 'rotate(0deg)');
+        PlusRotate6 = false;
+    }
+    $('#answer6').slideToggle(300);
+});
+
